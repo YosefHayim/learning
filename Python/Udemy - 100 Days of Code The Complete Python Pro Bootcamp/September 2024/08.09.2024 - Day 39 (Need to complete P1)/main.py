acb196ec181df00e1fefe664186c847d6817bee4
Yosef Hayim <yosefisabag+03@gmail.com>
2024-09-21 23:47:59 +0300

    d

--- Python/Udemy - 100 Days of Code The Complete Python Pro Bootcamp/September 2024/08.09.2024 - Day 39 (Need to complete P1)/main.py
+++
@@ -1,6 +0,0 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-from data_manager import sheet_data
-
-for i in sheet_data:
-    city = i['city']
-    print(city)
