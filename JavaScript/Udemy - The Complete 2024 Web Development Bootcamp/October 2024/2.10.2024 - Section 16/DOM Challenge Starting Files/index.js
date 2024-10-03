6e3cd428a9850b18f203598d20c31013a51269a3
Yosef Hayim <yosefisabag+03@gmail.com>
2024-10-03 16:01:14 +0300

    gdf

--- JavaScript/Udemy - The Complete 2024 Web Development Bootcamp/October 2024/2.10.2024 - Section 16/DOM Challenge Starting Files/index.js
+++ JavaScript/Udemy - The Complete 2024 Web Development Bootcamp/October 2024/2.10.2024 - Section 16/DOM Challenge Starting Files/index.js
@@ -4,4 +4,12 @@ thirdLi.textContent = 'This has been changed';
 let buttonClickMe = document.querySelector("button");
 buttonClickMe.style.backgroundColor = "yellow"
 
+let titleHello = document.querySelector("h1");
 
+titleHello.classList = "huge"
+
+// Makes this CSS property on
+// titleHello.classList.toggle("huge")
+
+// Re-calling it makes it off
+// titleHello.classList.toggle("huge")
