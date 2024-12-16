416d9547fc897120be1a1bf2ef1c28cd1f71b4be
Yosef Hayim <yosefisabag+03@gmail.com>
2024-12-16 22:17:57 +0200

    fd

--- JavaScript/udemy/master-node-by-building-a-real-world-restful-api-and-web-app-with-authentication-node-js-security-payments-more/section-5/index.js
+++ JavaScript/udemy/master-node-by-building-a-real-world-restful-api-and-web-app-with-authentication-node-js-security-payments-more/section-5/index.js
@@ -1,18 +1,40 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  if (err) return console.log(err);
-  console.log(`Breed: ${data}`);
+// Without promises
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   if (err) return console.log(err);
+//   console.log(`Breed: ${data}`);
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      console.log(res.body);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       console.log(res.body);
 
-      fs.writeFile("dog-img.txt", res.body.message, (err, data) => {
-        if (err) return console.log(err);
-        console.log("Random dog image saved to file!");
-      });
-    });
-});
+//       fs.writeFile("dog-img.txt", res.body.message, (err, data) => {
+//         if (err) return console.log(err);
+//         console.log("Random dog image saved to file!");
+//       });
+//     });
+// });
+
+//With promises
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   if (err) return console.log(err);
+//   const breed = data.toString().trim(); // Ensure proper string handling
+//   console.log(`Breed: ${breed}`);
+
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${breed}/images/random`)
+//     .then((res) => {
+//       console.log(res.body.message);
+
+//       fs.writeFile("dog-img.txt", res.body.message, (err) => {
+//         if (err) return console.log(err);
+//         console.log("Random dog image saved to file!");
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
