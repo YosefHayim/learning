8a9e34df94587da94702994e650e1d1383c90499
yosefhayim <yosefisabag+03@gmail.com>
2024-12-17 08:27:47 +0200

    re

--- JavaScript/udemy/master-node-by-building-a-real-world-restful-api-and-web-app-with-authentication-node-js-security-payments-more/section-4/modules.js
+++
@@ -1,9 +0,0 @@
-// console.log(arguments);
-// console.log(require("module").wrapper);
-const C = require("./test-module-1");
-
-const calc = new C();
-console.log(calc.add(2, 5));
-
-const calc2 = require("./test-module-2");
-console.log(calc2.multiply(10, 10));
