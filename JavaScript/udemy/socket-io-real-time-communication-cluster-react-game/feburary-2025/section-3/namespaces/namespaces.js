b553eb72c6094bc309bd5e90a72e184c9ccf01a9
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-10 16:29:36 +0200

    gf

---
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-3/namespaces/namespaces.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+// anything inside the public folder will render if somebody gets to the filenames.html
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(8000);
+
+// io refers to the entire socket server.
+const io = socketio(expressServer);
+
+//socket is always individual connection e.g. one user.
+io.on("connection", (socket) => {
+  console.log(socket.id, "has connected");
+
+  // in ws we use 'send' method and in socket.io we use the 'emit' method.
+  // socket.emit("messageFromServer", { data: "Welcome to the server" });
+
+  socket.on("newMessageToServer", (data) => {
+    io.emit("newMessageToClients", { text: data.text });
+  });
+});
