c00716cd4ec4819cbafa6bc455852b987582e5e6
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-10 13:01:36 +0200

    hg

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/socket-io-example/basics.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/socket-io-example/basics.js
@@ -7,3 +7,11 @@ app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(8000);
 const io = socketio(expressServer);
+io.on("connection", (socket) => {
+  console.log(socket.id, "has connected");
+  // in ws we use 'send' method and in socket.io we use the 'emit' method.
+  socket.emit("messageFromServer", { data: "Welcome to the server" });
+  socket.on("messageFromClient", (data) => {
+    console.log(data);
+  });
+});
