2fd5651385fb7ee2fca95b706986a69a32dedbee
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-10 13:17:50 +0200

    nice socket

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/socket-io-example/basics.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/socket-io-example/basics.js
@@ -6,11 +6,17 @@ const app = express();
 app.use(express.static(__dirname + "/public"));
 
 const expressServer = app.listen(8000);
+
+// io refers to the entire socket server.
 const io = socketio(expressServer);
+
+//socket is always individual connection e.g. one user.
 io.on("connection", (socket) => {
   console.log(socket.id, "has connected");
+
   // in ws we use 'send' method and in socket.io we use the 'emit' method.
   socket.emit("messageFromServer", { data: "Welcome to the server" });
+
   socket.on("messageFromClient", (data) => {
     console.log(data);
   });
