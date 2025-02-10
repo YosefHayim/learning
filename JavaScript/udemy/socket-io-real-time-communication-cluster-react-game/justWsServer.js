738d93211ecaae9abf6a422f4882b370c76d9d30
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-10 12:15:25 +0200

    gf

---
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/justWsServer.js
@@ -0,0 +1,21 @@
+// http is a core node module
+const http = require("http");
+// ws is a 3rd party module
+const websocket = require("ws");
+
+const server = http.createServer((req, res) => {
+  res.end("I am connected");
+});
+
+const wss = new websocket.WebSocketServer({ server });
+
+wss.on("headers", (headers, req) => {
+  // console.log(headers);
+});
+
+wss.on("connection", (ws, req) => {
+  // console.log(req);
+  ws.send("Welcome to the websocket server!!!");
+});
+
+server.listen(8000);
