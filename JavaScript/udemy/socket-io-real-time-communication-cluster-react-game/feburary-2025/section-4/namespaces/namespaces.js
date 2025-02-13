a5dbfad4466bb54abeb31b5323d8da3fd60d679b
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-13 18:48:15 +0200

    gfd

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-4/namespaces/namespaces.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-4/namespaces/namespaces.js
@@ -8,7 +8,11 @@ app.use(express.static(__dirname + "/public"));
 const expressServer = app.listen(8000);
 
 // io refers to the entire socket server.
-const io = socketio(expressServer);
+const io = socketio(expressServer, {
+  cors: {
+    origin: ["http://localhost:8000", "http://127.0.0.1:5500"],
+  },
+});
 
 //socket is always individual connection e.g. one user.
 io.on("connection", (socket) => {
@@ -17,7 +21,16 @@ io.on("connection", (socket) => {
   // in ws we use 'send' method and in socket.io we use the 'emit' method.
   // socket.emit("messageFromServer", { data: "Welcome to the server" });
 
+  // creating a new namespace named "newMessageToServer" and sending all the data
+  // on it to the entire server within "newMessageToClients"
   socket.on("newMessageToServer", (data) => {
     io.emit("newMessageToClients", { text: data.text });
+  });
+});
+
+io.of("/admin").on("connection", (socket) => {
+  console.log(socket.id, "has joined /admin");
+  io.of("/admin").emit("messageToClientsFromAdmin", {
+    text: "Welcome to the messages from admins from server",
   });
 });
