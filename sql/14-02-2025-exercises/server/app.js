d9354756aae687cb783f7a74609e7ccf8300c375
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-14 16:57:23 +0200

    gfd

--- sql/14-02-2025-exercises/server/app.js
+++ sql/14-02-2025-exercises/server/app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const sql = require("mssql");
+const connectDb = require("./config/connectDb");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+connectDb();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
