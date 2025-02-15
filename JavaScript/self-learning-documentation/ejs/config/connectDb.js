c7ffe0bbdfee9c8f585b081179401043739e62c7
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-15 19:36:31 +0200

    gdf

--- JavaScript/self-learning-documentation/ejs/config/connectDb.js
+++ JavaScript/self-learning-documentation/ejs/config/connectDb.js
@@ -1,16 +1,26 @@
 const sql = require("mssql");
 
+const sqlConfig = {
+  user: "sa",
+  password: "MichelleK2",
+  server: "localhost",
+  database: "StackOverflowMini",
+  options: {
+    encrypt: false, // Disable encryption temporarily
+    trustServerCertificate: true,
+  },
+  port: 1433,
+};
+
 const connectDb = async () => {
   try {
-    const isConnect = await sql.connect(
-      "Server=localhost,1433;Database=database;User Id=username;Password=password;Encrypt=true"
-    );
+    const isConnect = await sql.connect(sqlConfig);
 
     if (isConnect) {
-      console.log(`Successfully connected to DB: `, isConnect);
+      console.log(`Successfully connected to DB: `, isConnect.config.database);
     }
   } catch (err) {
-    console.error(`Error durning connecting to sql server: `, err);
+    console.error(`Error during connecting to SQL server: `, err);
   }
 };
 
