8757fa2ed936b08870d5f37c4e378547c29e7c7a
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-23 15:35:36 +0200

    Set up canvas and UI interaction for game login and spawn modals

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/client/uiStuff.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/client/uiStuff.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log("sanity check");
+// set height and width of canvas = window
+
+let wHeight = window.innerHeight;
+let wWidth = window.innerWidth;
+const player = {}; // This will be all things "this" player
+
+// canvas element needs to be in a variable
+const canvas = document.querySelector("#the-canvas");
+
+// context is how we draw! we will be drawing in 2 d
+const context = canvas.getContext("2d");
+
+// Set the canvas height and width to = window height and width
+canvas.height = wHeight;
+canvas.width = wWidth;
+
+// Put the modals in variables so we can interact with them.
+window.addEventListener("load", () => {
+  const loginModal = new bootstrap.Modal(document.querySelector("#loginModal"));
+  const spawnModel = new bootstrap.Modal(document.querySelector("#spawnModal"));
+  loginModal.show();
+});
+
+document.querySelector(".name-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  // console.log("Submitted!");
+  player.name = document.querySelector("#name-input").value;
+  loginModal.hide();
+  spawnModal.show();
+});
