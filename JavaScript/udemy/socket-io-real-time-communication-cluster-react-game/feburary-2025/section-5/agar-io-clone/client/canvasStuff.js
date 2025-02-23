1105ddc650465ce61dbef88ea6781798893b276f
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-23 16:44:38 +0200

    Implement player movement and mouse tracking for canvas interaction

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/client/canvasStuff.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/client/canvasStuff.js
@@ -7,8 +7,9 @@ const init = () => {
 // ============DRAW=========
 // =========================
 
-let randomX = Math.floor(500 * Math.random() + 10);
-let randomY = Math.floor(500 * Math.random() + 10);
+player.lockX = Math.floor(500 * Math.random() + 10);
+player.lockY = Math.floor(500 * Math.random() + 10);
+
 context.beginPath();
 context.fillStyle = "rgb(255,0,0)";
 context.arc(randomX, randomY, 10, 0, Math.PI * 2); // draw an arc/circle
@@ -21,4 +22,63 @@ context.lineWidth = 3; // how wide to draw a line in pixels
 context.strokeStyle = "rgb(0,255,0)"; // draw a green line
 context.stroke();
 
-const draw = () => {};
+const draw = () => {
+  context.beginPath();
+  context.fillStyle = "rgb(255,0,0)";
+  context.arc(player.lockX, player.lockY, 10, 0, Math.PI * 2); // draw an arc/circle
+  // arg1 and arg2 are center x and center of the arc
+  // arg3 = radius of the the circle
+  // arg4 = where to start drawing in the radians - 0 = 3:00
+  // arg 5 = where to stop drawing in the radians - Pi = 90deg
+  context.fill();
+  context.lineWidth = 3; // how wide to draw a line in pixels
+  context.strokeStyle = "rgb(0,255,0)"; // draw a green line
+  context.stroke();
+  // requestAnimationFrame is like a controlled loop.
+  // It runs recursively
+  requestAnimationFrame(draw);
+};
+
+canvas.addEventListener("mousemove", (event) => {
+  const mousePosition = {
+    x: event.clientX,
+    y: event.clientY,
+  };
+  const angleDeg =
+    (Math.atan2(
+      mousePosition.y - canvas.height / 2,
+      mousePosition.x - canvas.width / 2
+    ) *
+      180) /
+    Math.PI;
+  if (angleDeg >= 0 && angleDeg < 90) {
+    xVector = 1 - angleDeg / 90;
+    yVector = -(angleDeg / 90);
+    console.log("Mouse is in the lower right quadrant");
+  } else if (angleDeg >= 90 && angleDeg <= 180) {
+    xVector = -(angleDeg - 90) / 90;
+    yVector = -(1 - (angleDeg - 90) / 90);
+    console.log("Mouse is in the lower left quadrant");
+  } else if (angleDeg >= -180 && angleDeg < -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 + (angleDeg + 90) / 90;
+    console.log("Mouse is in the top left quadrant");
+  } else if (angleDeg < 0 && angleDeg >= -90) {
+    xVector = (angleDeg + 90) / 90;
+    yVector = 1 - (angleDeg + 90) / 90;
+    console.log("Mouse is in the top right quadrant");
+  }
+
+  speed = 10;
+  xV = xVector;
+  yV = yVector;
+
+  if ((player.locX < 5 && xV < 0) || (player.locX > 500 && xV > 0)) {
+    player.locY -= speed * yV;
+  } else if ((player.locY < 5 && yV > 0) || (player.locY > 500 && yV < 0)) {
+    player.locX += speed * xV;
+  } else {
+    player.locX += speed * xV;
+    player.locY -= speed * yV;
+  }
+});
