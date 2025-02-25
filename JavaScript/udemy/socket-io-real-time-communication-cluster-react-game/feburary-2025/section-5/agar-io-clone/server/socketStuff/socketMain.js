dcedeb5991e4f93d4016ac27fa0ce38b0fb4fde1
Yosef Hayim <yosefisabag+03@gmail.com>
2025-02-25 19:27:13 +0200

    Add player configuration and data classes for game initialization

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/server/socketStuff/socketMain.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-5/agar-io-clone/server/socketStuff/socketMain.js
@@ -4,6 +4,8 @@ const io = require("../servers").io;
 const app = require("../servers").app;
 
 const Orb = require("./classes/Orb");
+const PlayerConfig = require("./classes/PlayerConfig");
+const PlayerData = require("./classes/PlayerData");
 // Make an orbs array that will host all 500/5000 NOT PLAYER orbs.
 // every time one is absorb, the server will make a new one
 
@@ -24,10 +26,12 @@ initGame();
 io.on("connect", (socket) => {
   // A player hs been connected
   // event that runs on join that does init game stuff
+  // Make a playerConfig object - the data specific to this player that only the player needs to know
+  const playerConfig = new PlayerConfig(settings);
+  const playerData = new PlayerData();
+  // Make a playerData object - the data specific to this player that everyone needs to know
+  // a Master player object to house both
   socket.emit("init", {
-    // Make a playerConfig object - the data specific to this player that only the player needs to know
-    // Make a playerData object - the data specific to this player that everyone needs to know
-    // a Master player object to house both
     orbs,
   });
 });
