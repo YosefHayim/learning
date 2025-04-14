605eb81b7850783c1a80479c315d1fc847088271
yosefhayim <yosefisabag+03@gmail.com>
2025-04-14 10:05:13 +0300

    continue learn socket

--- JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-6/prefload/server/servers.js
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-6/prefload/server/servers.js
@@ -8,3 +8,66 @@
 // Entrypoint for our cluster which will make workers
 // And the workers will do the socket.io handling
 // See https://github.com/elad/node-cluster-socket.io
+
+const cluster = require("cluster"); // make it so we can use multiple threads
+const http = require("http"); // if we need express we will implemenet a different way.
+const { Server } = require("socket.io");
+const numCPUs = require("os").cpus().length;
+const { setupMaster, setupWorker } = require("@socket.io/sticky"); // sticky makes it so the client can find the way back correct worker
+const { createAdapter, setupPrimary } = require("@socket.io/cluster-adapter"); // make it the primpary node can emit to everyone
+
+if (cluster.isPrimary) {
+  console.log(`Primary ${process.pid} is running`);
+
+  const httpServer = http.createServer();
+
+  // setup sticky sessions
+  setupMaster(httpServer, {
+    loadBalancingMethod: "least-connection",
+  });
+
+  // setup connections between the workers
+  setupPrimary();
+
+  // needed for packets containing buffers (you can ignore it if you only send plaintext objects)
+  // Node.js < 16.0.0
+  cluster.setupMaster({
+    serialization: "advanced",
+  });
+  // Node.js > 16.0.0
+  // cluster.setupPrimary({
+  //   serialization: "advanced",
+  // });
+
+  httpServer.listen(3000); // internet facing
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  const httpServer = http.createServer();
+  const io = new Server(httpServer, {
+    cors: {
+      origin: "http://localhost:5173",
+      credentials: true,
+    },
+  });
+
+  // use the cluster adapter
+  io.adapter(createAdapter()); // changes from the default adapater
+
+  // setup connection with the primary process
+  setupWorker(io);
+
+  io.on("connection", (socket) => {
+    console.log(`Someone connected to this worker: ${process.pid}`);
+    socket.emit("welcome", "Welcome to our cluster driven socket.io server!");
+  });
+}
---
+++ JavaScript/udemy/socket-io-real-time-communication-cluster-react-game/feburary-2025/section-6/react-client/src/socketConnection.js
@@ -0,0 +1,6 @@
+import io from "socket-io.client";
+console.log("File loaded");
+const socket = io.connect("http://localhost:3000"); // our server is at :3000
+socket.on("welcome", (data) => {
+  console.log(data);
+});
