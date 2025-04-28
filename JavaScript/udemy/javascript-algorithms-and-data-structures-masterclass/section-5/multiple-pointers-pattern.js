3691ca75e88d9f3ad51a97c51b41a78f03717163
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-28 12:10:18 +0300

    refactored name of files an working on multi pointers pattern

---
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/section-5/multiple-pointers-pattern.js
@@ -0,0 +1,23 @@
+// write a function called sumZero which accepts a sorted array of integers.
+// The Function should find the first pair where the sum is 0. return an array that includes
+// both values that sum to 0 or undefined if a pair does not exist.
+
+// example sumZero([-3,-2,-1,0,1,2,3]) // result is [-3,3]
+
+const sumZero = (arr) => {
+  // 1. for each index within the array sum with the next index.
+  for (let i = 0; i < arr.length; i++) {
+    let n = arr[i];
+    let nextN = arr[i + 1];
+    console.log(`Numbers being added: `, n, nextN);
+    if (n + nextN === 0) {
+      let result = { n, nextN };
+      console.log(result);
+      return result;
+    }
+    return undefined;
+  }
+  // 2. first sum that gets to 0 return the result else return undefined.
+};
+
+sumZero([-3, -2, -1, 0, 1, 2, 3]);
