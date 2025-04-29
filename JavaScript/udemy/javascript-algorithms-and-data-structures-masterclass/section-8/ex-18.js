1c6722b343a1cd25539168bea25c4ee2b75202dd
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-29 15:02:39 +0300

    ex 19

---
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/section-8/ex-18.js
@@ -0,0 +1,18 @@
+// productOfArray
+// Write a function called productOfArray which takes in an
+// array of numbers and returns the product
+// of them all.
+
+// failed to understand
+// correct
+const productOfArray = (arr) => {
+  if (arr.length === 0) {
+    return 1;
+  }
+  const res = arr[0] * productOfArray(arr.slice(1));
+  console.log(res);
+  return res;
+};
+
+productOfArray([1, 2, 3]); // 6
+productOfArray([1, 2, 3, 10]); // 60
