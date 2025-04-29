b4ae18cbdcc39e752542931ec90ad98cf98057c2
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-29 18:08:30 +0300

    passed recursive first

--- JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/section-9/ex-21.js
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/section-9/ex-21.js
@@ -2,15 +2,23 @@
 // Write a recursive function called reverse which
 // accepts a string and returns a new string in reverse.
 
-// correct 100/100
-const reverse = (str) => {
-  let wordReversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    const letter = str[i];
-    wordReversed += letter;
-  }
-  console.log(wordReversed);
-  return wordReversed;
+// not recursive version but succeed!
+// const reverse = (str) => {
+//   let wordReversed = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     const letter = str[i];
+//     wordReversed += letter;
+//   }
+//   console.log(wordReversed);
+//   return wordReversed;
+// };
+
+// score 77/100
+const reverseRecursive = (str) => {
+  if (str.length === 0) return ""; // My error forgot to return empty string, instead of undefined
+  const lastLetter = str[str.length - 1];
+  const withoutLastLetter = str.slice(0, str.length - 1);
+  return lastLetter + reverseRecursive(withoutLastLetter);
 };
 
-reverse("awesome");
+console.log(reverseRecursive("awesome"));
