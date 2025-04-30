8e9b201f6d6ff673eb9c8092d86017e58a396198
Yosef Hayim <yosefisabag+03@gmail.com>
2025-04-30 23:27:12 +0300

    learning binary search

---
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/section-10/ex-32.js
@@ -0,0 +1,15 @@
+// Create a function that takes two strings:
+// A longer string (e.g. "hellohello")
+// A shorter pattern to find (e.g. "lo")
+// And returns how many times the shorter pattern appears in the longer string.
+
+const stringSearch = (word, pattern) => {
+  // error handling
+
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+  }
+};
+
+stringSearch("wowomgzomg", "omg"); // expected result: 2
+// stringSearch("aaaaaa", "aa"); // expected result: 5
