23349029e009cdc9aa624ce30a1df8bd822e3abe
Yosef Hayim <yosefisabag+03@gmail.com>
2025-05-02 19:03:11 +0300

    working and continue solve algos

---
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/validParenthesis.js
@@ -0,0 +1,13 @@
+// =============================
+// 2️⃣ Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+// determine if the input string is valid.
+//
+// Example:
+// Input: s = "()[]{}"
+// Output: true
+function isValid(s) {
+  // TODO: implement
+  return false;
+}
+// console.log(isValid("()[]{}")); // Expected: true
