b05310383011d0d220f523a501503edf62121997
yosefhayim <yosefisabag+03@gmail.com>
2025-05-02 22:20:56 +0300

    learning

--- JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/other-exercises.js
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/other-exercises.js
@@ -1,109 +1,23 @@
-// =============================
-// 3️⃣ Merge Two Sorted Lists
-// Merge two sorted linked lists and return it as a new sorted list.
-//
-// Example:
-// Input: l1 = [1, 2, 4], l2 = [1, 3, 4]
-// Output: [1, 1, 2, 3, 4, 4]
-function mergeTwoLists(l1, l2) {
-  // TODO: implement
-  return null;
-}
-// console.log(mergeTwoLists([1,2,4], [1,3,4])); // Expected: [1,1,2,3,4,4]
+var mergeTwoLists = function (list1, list2) {
+  if (list1.length === 0 && list2.length === 0) {
+    console.log("[]");
+    return [];
+  }
 
-// =============================
-// 4️⃣ Best Time to Buy and Sell Stock
-// Given an array prices where prices[i] is the price of a given stock on day i,
-// find the maximum profit you can achieve from one transaction.
-//
-// Example:
-// Input: prices = [7,1,5,3,6,4]
-// Output: 5
-function maxProfit(prices) {
-  // TODO: implement
-  return 0;
-}
-// console.log(maxProfit([7,1,5,3,6,4])); // Expected: 5
+  let longArr = [];
 
-// =============================
-// 5️⃣ Valid Anagram
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-//
-// Example:
-// Input: s = "anagram", t = "nagaram"
-// Output: true
-function isAnagram(s, t) {
-  // TODO: implement
-  return false;
-}
-// console.log(isAnagram("anagram", "nagaram")); // Expected: true
+  for (let i = 0; i < list1.length; i++) {
+    const n = list1[i];
+    longArr.push(n);
+  }
 
-// =============================
-// 6️⃣ Climbing Stairs
-// You are climbing a staircase. It takes n steps to reach the top.
-// Each time you can climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-//
-// Example:
-// Input: n = 3
-// Output: 3
-function climbStairs(n) {
-  // TODO: implement
-  return 0;
-}
-// console.log(climbStairs(3)); // Expected: 3
-
-// =============================
-// 7️⃣ Maximum Subarray (Kadane’s Algorithm)
-// Find the contiguous subarray (containing at least one number) which has the largest sum.
-//
-// Example:
-// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-// Output: 6
-function maxSubArray(nums) {
-  // TODO: implement
-  return 0;
-}
-// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])); // Expected: 6
-
-// =============================
-// 8️⃣ Binary Search
-// Given a sorted array of integers nums and a target value, return the index if the target is found.
-// If not, return -1.
-//
-// Example:
-// Input: nums = [-1,0,3,5,9,12], target = 9
-// Output: 4
-function binarySearch(nums, target) {
-  // TODO: implement
-  return -1;
-}
-// console.log(binarySearch([-1,0,3,5,9,12], 9)); // Expected: 4
-
-// =============================
-// 9️⃣ Flood Fill
-// An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
-// Perform a "flood fill" starting from (sr, sc) with the newColor.
-//
-// Example:
-// Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
-// Output: [[2,2,2],[2,2,0],[2,0,1]]
-function floodFill(image, sr, sc, newColor) {
-  // TODO: implement
-  return [];
-}
-// console.log(floodFill([[1,1,1],[1,1,0],[1,0,1]], 1, 1, 2));
-// Expected: [[2,2,2],[2,2,0],[2,0,1]]
-
-// =============================
-// 🔟 Permutations
-// Given an array nums of distinct integers, return all possible permutations.
-//
-// Example:
-// Input: nums = [1,2,3]
-// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
-function permute(nums) {
-  // TODO: implement
-  return [];
-}
-// console.log(permute([1,2,3]));
-// Expected: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+  for (let j = 0; j < list2.length; j++) {
+    const n = list2[j];
+    longArr.push(n);
+  }
+  longArr.sort((a, b) => a - b);
+  console.log(longArr);
+  return longArr;
+};
+mergeTwoLists([1, 2, 4], [1, 3, 4]);
+mergeTwoLists([], []);
