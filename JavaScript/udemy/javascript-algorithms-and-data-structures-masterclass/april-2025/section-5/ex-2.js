4bf35f4aa6128a90af955a0edd8ebe28ce127c3a
Yosef Hayim <yosefisabag+03@gmail.com>
2025-05-03 17:19:44 +0300

    succeed sec ex

---
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/april-2025/section-5/ex-2.js
@@ -0,0 +1,25 @@
+// Multiple Pointers - countUniqueValues
+// Implement a function called countUniqueValues, which accepts a sorted array,
+// and counts the unique values in the array. There can be negative numbers in
+// the array, but it will always be sorted.
+
+const countUniqueValues = (arr) => {
+  const end = arr.length - 1;
+  if (arr[0] > arr[end]) {
+    throw new Error("This is not sorted");
+  }
+  const removedDuplicates = new Set(arr);
+  console.log(removedDuplicates.size);
+  return removedDuplicates.size;
+};
+countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+countUniqueValues([]); // 0
+countUniqueValues([-2, -1, -1, 0, 1]); // 4
+// Time Complexity - O(n)
+
+// Space Complexity - O(n)
+
+// Bonus
+
+// You must do this with constant or O(1) space and O(n) time.
