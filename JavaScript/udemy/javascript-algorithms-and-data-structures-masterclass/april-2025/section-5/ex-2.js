3a0a962a8fdf64f208650de3723ee9cbbc35c34b
Yosef Hayim <yosefisabag+03@gmail.com>
2025-05-03 18:56:06 +0300

    continue to learn slide window an divide andd conquer again

--- JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/april-2025/section-5/ex-2.js
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/april-2025/section-5/ex-2.js
@@ -16,24 +16,24 @@
 // };
 
 // With O(n) and space of O(1)
-const countUniqueValues = (arr) => {
-  if (arr.length === 0) return 0;
+// const countUniqueValues = (arr) => {
+//   if (arr.length === 0) return 0;
 
-  let uniqueCounts = 1; // start at 1 because the first value is unique by default
+//   let uniqueCounts = 1; // start at 1 because the first value is unique by default
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] !== arr[i - 1]) {
-      uniqueCounts++;
-    }
-  }
+//   for (let i = 1; i < arr.length; i++) {
+//     if (arr[i] !== arr[i - 1]) {
+//       uniqueCounts++;
+//     }
+//   }
 
-  console.log(uniqueCounts);
-  return uniqueCounts;
-};
-countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
-countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
-countUniqueValues([]); // 0
-countUniqueValues([-2, -1, -1, 0, 1]); // 4
+//   console.log(uniqueCounts);
+//   return uniqueCounts;
+// };
+// countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
+// countUniqueValues([]); // 0
+// countUniqueValues([-2, -1, -1, 0, 1]); // 4
 // Time Complexity - O(n)
 // Space Complexity - O(n)
 
