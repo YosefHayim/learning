165146bb0b59f242794811170d5b5b2ea3032380
Yosef Hayim <yosefisabag+03@gmail.com>
2025-05-04 01:32:49 +0300

    finish

--- JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/may-2025/section-5/ex-2.js
+++ JavaScript/udemy/javascript-algorithms-and-data-structures-masterclass/may-2025/section-5/ex-2.js
@@ -4,7 +4,6 @@
 // the array, but it will always be sorted.
 
 // With O(n) of time and space
-
 const countUniqueValues2 = (arr) => {
   const end = arr.length - 1;
   if (arr[0] > arr[end]) {
@@ -34,8 +33,3 @@ countUniqueValues([1, 1, 1, 1, 1, 2]); // 2
 countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]); // 7
 countUniqueValues([]); // 0
 countUniqueValues([-2, -1, -1, 0, 1]); // 4
-// Time Complexity - O(n)
-// Space Complexity - O(n)
-
-// Bonus
-// You must do this with constant or O(1) space and O(n) time.
