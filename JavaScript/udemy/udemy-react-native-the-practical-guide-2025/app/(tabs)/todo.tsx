d23d6719b0b7a2c65f5be966ca6df37941763faa
yosefhayim <yosefisabag+03@gmail.com>
2025-06-04 10:49:25 +0300

    success doing simple todo list

--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/app/(tabs)/todo.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/app/(tabs)/todo.tsx
@@ -1,19 +1,37 @@
 import CustomInput from "@/components/CustomInput/CustomInput";
 import LineSeperator from "@/components/LineSeperator";
 import useCurrentDate from "@/hooks/useCurrentDate";
+import { useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 
 const Todo = () => {
   const formattedDate = useCurrentDate();
-  
+
+  const [goals, setGoals] = useState<string[]>([]);
+  const [goalText, setGoalText] = useState("");
+
+  const handleChangeInput = (newGoalText?: string | undefined): void => {
+    if (!newGoalText) return;
+    setGoalText(newGoalText);
+  };
+
+  const handlePressSubmitBtn = () => {
+    setGoals([...goals, goalText]);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Goals of today: {formattedDate}</Text>
       <View style={styles.btnAndInput}>
-        <CustomInput placeholder="Add a goal" placeholderTextColor="#888" />
-        <Button title="Add Goal" />
+        <CustomInput placeholder="Add a goal" placeholderTextColor="#888" onChangeFn={handleChangeInput} />
+        <Button title="Add Goal" onPress={handlePressSubmitBtn} />
       </View>
       <LineSeperator />
+      {goals.map((goal) => (
+        <View key={goal}>
+          <Text>{goal}</Text>
+        </View>
+      ))}
     </View>
   );
 };
--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/components/CustomInput/CustomInput.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/components/CustomInput/CustomInput.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { StyleSheet, TextInput } from "react-native";
 
-const CustomInput: React.FC<{ placeholder: string; placeholderTextColor: string }> = ({ placeholder, placeholderTextColor }) => {
-  return <TextInput style={styles.input} placeholder={placeholder} placeholderTextColor={placeholderTextColor} />;
+type Props = {
+  placeholder: string;
+  placeholderTextColor: string;
+  onChangeFn: () => void;
+};
+
+const CustomInput: React.FC<Props> = ({ placeholder, placeholderTextColor, onChangeFn }) => {
+  return <TextInput style={styles.input} placeholder={placeholder} placeholderTextColor={placeholderTextColor} onChangeText={onChangeFn} />;
 };
 
 const styles = StyleSheet.create({
