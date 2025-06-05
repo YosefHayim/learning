ca1861cb1fd4355d3831ef4c64d1b3bea30ae4ff
yosefhayim <yosefisabag+03@gmail.com>
2025-06-05 11:06:39 +0300

    organize colors to the recommended pattern by exp doc

--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/components/PrimaryButton.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/components/PrimaryButton.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from "react";
 import { View, Text, StyleSheet, Pressable, PressableProps } from "react-native";
+import { Colors } from "../constants/colors";
 
 const PrimaryButton: React.FC<{ children: ReactNode; onPress: () => void }> = ({ children, onPress }) => {
   return (
-    <View style={{ width: "100%", padding: 10 }}>
-      <Pressable onPress={onPress}>
-        <Text style={{ color: "white", backgroundColor: "orange", textAlign: "center", borderRadius: 4, padding: 8 }}>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={onPress}
+        android_ripple={{ color: Colors.primary600 }}
+        style={({ pressed }) => (pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer)}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
     </View>
   );
@@ -13,4 +18,23 @@ const PrimaryButton: React.FC<{ children: ReactNode; onPress: () => void }> = ({
 
 export default PrimaryButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/StartGameScreen.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/StartGameScreen.tsx
@@ -2,6 +2,7 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
+import { Colors } from "../constants/colors";
 
 const StartGameScreen: React.FC<{ setUserNumber: React.Dispatch<React.SetStateAction<string>> }> = ({ setUserNumber }) => {
   const [enteredNumber, setEnteredNumber] = useState<string>("");
@@ -46,7 +47,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -58,9 +59,9 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
