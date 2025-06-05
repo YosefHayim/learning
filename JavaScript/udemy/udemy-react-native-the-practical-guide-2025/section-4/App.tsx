e405dadecf62184524bb3f9e9cc087ed4a3e756c
yosefhayim <yosefisabag+03@gmail.com>
2025-06-05 10:18:40 +0300

    63 section 4

--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/App.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
@@ -17,7 +17,7 @@ export default function App() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
       <ImageBackground source={require("./assets/image.png")} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/GameScreen.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/GameScreen.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Title from "../components/Title";
 
 const GameScreen = () => {
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.screen}>
+      <Title titleText="Opponent׳s Guess" />
+      {/* Guess */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + - */}
+      </View>
+      {/* Log rounds */}
     </View>
   );
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+    marginTop: 100,
+  },
+});
