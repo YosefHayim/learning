59eff7bf133879b8730056102d697e7de29d4ca2
yosefhayim <yosefisabag+03@gmail.com>
2025-06-05 10:02:08 +0300

    continue learning react

--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/components/PrimaryButton.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/components/PrimaryButton.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable, PressableProps } from "react-native";
 
-const PrimaryButton: React.FC<{ children: ReactNode }> = ({ children }) => {
+const PrimaryButton: React.FC<{ children: ReactNode; onPress: () => void }> = ({ children, onPress }) => {
   return (
     <View style={{ width: "100%", padding: 10 }}>
-      <Text style={{ color: "white", backgroundColor: "orange", textAlign: "center", borderRadius: 4, padding: 8 }}>{children}</Text>
+      <Pressable onPress={onPress}>
+        <Text style={{ color: "white", backgroundColor: "orange", textAlign: "center", borderRadius: 4, padding: 8 }}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/StartGameScreen.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-4/screens/StartGameScreen.tsx
@@ -1,17 +1,36 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 
 import PrimaryButton from "../components/PrimaryButton";
+import { useState } from "react";
 
 function StartGameScreen() {
+  const [enteredNumber, setEnteredNumber] = useState<string>("");
+
+  const confirmInputHandler = () => {
+    const chosneNumber = parseInt(enteredNumber);
+    if (isNaN(chosneNumber) || chosneNumber <= 0) {
+      Alert.alert("Invalid number", "number must be between 1 and 99.", [{ text: "okay", style: "destructive", onPress: () => setEnteredNumber("") }]);
+    }
+    console.log("valid number provided", enteredNumber);
+  };
+
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={enteredNumber}
+        onChangeText={(numberProvided) => setEnteredNumber(numberProvided)}
+      />
       <View style={styles.buttonsContainer}>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Reset</PrimaryButton>
+          <PrimaryButton onPress={() => setEnteredNumber("")}>Reset</PrimaryButton>
         </View>
         <View style={styles.buttonContainer}>
-          <PrimaryButton>Confirm</PrimaryButton>
+          <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
         </View>
       </View>
     </View>
