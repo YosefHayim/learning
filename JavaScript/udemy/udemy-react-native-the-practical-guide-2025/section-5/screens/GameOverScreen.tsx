a82a785e927415de4da20359306112da03294e9f
yosefhayim <yosefisabag+03@gmail.com>
2025-06-07 20:35:57 +0300

    learned about platform api thaht gets the device os and by using that can create a dynamic files of files either ended with ios or with android or with using the platform api

--- JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-5/screens/GameOverScreen.tsx
+++ JavaScript/udemy/udemy-react-native-the-practical-guide-2025/section-5/screens/GameOverScreen.tsx
@@ -1,20 +1,39 @@
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Image, ScrollView, StyleSheet, Text, useWindowDimensions, View } from "react-native";
 import Title from "../components/ui/Title";
 import { Colors } from "../constants/colors";
 import PrimaryButton from "../components/ui/PrimaryButton";
 
 const GameOverScreen: React.FC<{ roundsNumber: number | null; userNumber: number; onStartNewGame: () => void }> = ({ roundsNumber, userNumber, onStartNewGame }) => {
+  const { height, width } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 400) {
+    imageSize = 80;
+  }
+
+  const imageStyle = {
+    height: imageSize,
+    width: imageSize,
+    borderRaidus: imageSize / 2,
+  };
   return (
-    <View style={styles.rootContainer}>
-      <Title titleText="GAME OVER" />
-      <View style={styles.imageContainer}>
-        <Image source={require("../assets/success.png")} style={styles.image} />
+    <ScrollView style={{ flex: 1 }}>
+      <View style={styles.rootContainer}>
+        <Title titleText="GAME OVER" />
+        <View style={[styles.imageContainer, imageStyle]}>
+          <Image source={require("../assets/success.png")} style={styles.image} />
+        </View>
+        <Text style={styles.summaryText}>
+          Your phone needed <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlightText}>{userNumber}</Text>.
+        </Text>
+        <PrimaryButton onPress={onStartNewGame}>Start new game</PrimaryButton>
       </View>
-      <Text style={styles.summaryText}>
-        Your phone needed <Text style={styles.highlightText}>{roundsNumber}</Text> rounds to guess the number <Text style={styles.highlightText}>{userNumber}</Text>.
-      </Text>
-      <PrimaryButton onPress={onStartNewGame}>Start new game</PrimaryButton>
-    </View>
+    </ScrollView>
   );
 };
 
