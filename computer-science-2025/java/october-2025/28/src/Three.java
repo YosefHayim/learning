4c5612a53093597879ce37a57bed4d5120bc2e60
yosefhayim <yosefisabag+03@gmail.com>
2025-10-28 18:41:09 +0200

    .

---
+++ computer-science-2025/java/october-2025/28/src/Three.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Three {
+
+    public static void main (String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Provide 3 digits number: ");
+
+        String word = scan.nextLine();
+        System.out.println(word.charAt(0));
+        System.out.println(word.charAt(1));
+        System.out.println(word.charAt(2));
+
+//        הגדרת משתנה
+//
+//        קלט לתוך המשתנה
+//
+//
+//        הדפסת אחדות
+//
+//        הדפסת עשרות
+//
+//        הדפסת מאות
+
+    }
+
+}
